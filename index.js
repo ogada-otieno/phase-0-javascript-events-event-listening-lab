feccf402bf42e854c2792f6acaf62d8e3ee750d2
Rodgers Ogada <ogadarodgers@gmail.com>
2022-11-20 22:31:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+/*
+const input = document.getElementById("button");
+
 function addingEventListener() {
+    alert('I was clicked!');
+    }
+
+input.addEventListener('click', addingEventListener);
+*/
+
+
+
+
+const input = document.getElementById('button');
+
+function addingEventListener() {
+    input.addEventListener("click", function() {
+        alert("I was clicked!");
+    });
 }
+
+addingEventListener()
